db9ab0c023227060a034f6f8a8bc827ea65eb9da
derwaro <chrdina@gmail.com>
2023-04-09 22:30:46 -0600

    add tailwindcss/forms

--- appointment/forms.py
+++ appointment/forms.py
@@ -6,9 +6,15 @@ from phonenumber_field.formfields import PhoneNumberField
 
 class ChooseTreatmentsForm(forms.Form):
     name = forms.ModelChoiceField(queryset=Treatment.objects.filter(active=True).all())
-    client_name = forms.CharField(max_length=150)
-    client_surname = forms.CharField(max_length=150)
-    client_mail = forms.EmailField(required=True)
+    client_name = forms.CharField(
+        max_length=150,
+    )
+    client_surname = forms.CharField(
+        max_length=150,
+    )
+    client_mail = forms.EmailField(
+        required=True,
+    )
     client_phone = PhoneNumberField()
 
 
