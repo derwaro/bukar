a445163efb3c6ea83386ce212fe389a5b9504121
derwaro <chrdina@gmail.com>
2023-05-11 22:45:06 -0600

    add ClientSettings for clients/users

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import ClientSettings
+
 
 # Register your models here.
+class ClientSettingsAdmin(admin.ModelAdmin):
+    def get_queryset(self, request):
+        qs = super(ClientSettingsAdmin, self).get_queryset(request)
+        if request.user.is_superuser:
+            return qs
+        return qs.filter(user=request.user)
+
+    def save_model(self, request, obj, form, change):
+        if not obj.user:
+            obj.user = request.user
+        obj.save()
+
+
+admin.site.register(ClientSettings, ClientSettingsAdmin)
--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
+from django.contrib.auth.models import User
+
 # Create your models here.
+
+
+class ClientSettings(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    company_name = models.CharField(max_length=150)
+    address = models.CharField(max_length=250)
+    servable_client = models.PositiveIntegerField()
