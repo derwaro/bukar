ec390a477002a226e53fd54130faa4ec2ba3859f
derwaro <chrdina@gmail.com>
2023-05-24 17:39:06 -0600

    limit admin view queryset based on current logged in user

--- appointment/admin.py
+++ appointment/admin.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 
 from .models import Treatment
+from django.contrib.auth.models import Group, User
 
 
 class TreatmentAdmin(admin.ModelAdmin):
+    """
     def get_queryset(self, request):
         qs = super(TreatmentAdmin, self).get_queryset(request)
         if request.user.is_superuser:
@@ -14,6 +16,21 @@ class TreatmentAdmin(admin.ModelAdmin):
         if not obj.user:
             obj.user = request.user
         obj.save()
+    """
+
+    def get_queryset(self, request):
+        # limit visible Treatment objects to those create by currently logged in user
+        # if current user is in group "client"
+        client_group = Group.objects.get(name="client")
+        if "client" in list(request.user.groups.values_list("name", flat=True)):
+            qs = super(TreatmentAdmin, self).get_queryset(request)
+            return qs.filter(user=request.user)
+        # if current user is in group superuser, return all objects
+        elif request.user.username in list(
+            User.objects.filter(is_superuser=True).values_list("username", flat=True)
+        ):
+            qs = super(TreatmentAdmin, self).get_queryset(request)
+            return qs
 
 
 admin.site.register(Treatment, TreatmentAdmin)
