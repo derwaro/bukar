25ec9f71d99b4082745e7085d0270a946cd27b8a
derwaro <chrdina@gmail.com>
2023-05-25 10:10:24 -0600

    new Treatment assigned to current user

--- appointment/admin.py
+++ appointment/admin.py
@@ -21,5 +21,9 @@ class TreatmentAdmin(admin.ModelAdmin):
             qs = super(TreatmentAdmin, self).get_queryset(request)
             return qs
 
+    def save_model(self, request, obj, form, change):
+        obj.user = request.user
+        super().save_model(request, obj, form, change)
+
 
 admin.site.register(Treatment, TreatmentAdmin)
