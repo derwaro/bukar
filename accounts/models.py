1fbcc7f665552e38234d4de3e0a2da56210b5596
derwaro <chrdina@gmail.com>
2023-05-30 12:09:55 -0600

    add slug field to ClientSetting

--- accounts/models.py
+++ accounts/models.py
@@ -1,12 +1,23 @@
 from django.db import models
 
 from django.contrib.auth.models import User
+from django.template.defaultfilters import slugify
 
 # Create your models here.
 
 
 class ClientSetting(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    company_name = models.CharField(max_length=150)
+    company_name = models.CharField(max_length=150, unique=True)
+    company_name_slug = models.SlugField(
+        max_length=150, default=slugify(company_name), editable=False, unique=True
+    )
     address = models.CharField(max_length=250)
     servable_client = models.PositiveIntegerField()
+
+    def __str__(self):
+        return f"{self.user} with <{self.company_name}> at <{self.company_name_slug}>"
+
+    def save(self, *args, **kwargs):
+        self.company_name_slug = slugify(self.company_name)
+        super(ClientSetting, self).save(*args, **kwargs)
