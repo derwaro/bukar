b0718bc331090ab222c1d688d8d9aed3ee80b4e7
derwaro <chrdina@gmail.com>
2023-06-05 22:49:58 -0600

    add ClientSetting for client calendar setup

--- accounts/models.py
+++ accounts/models.py
@@ -14,10 +14,13 @@ class ClientSetting(models.Model):
     )
     address = models.CharField(max_length=250)
     servable_client = models.PositiveIntegerField()
+    calendar_id = models.CharField(max_length=90)
+    calendar_email = models.EmailField(default="email@example.com")
 
     def __str__(self):
         return f"{self.user} with <{self.company_name}> at <{self.company_name_slug}>"
 
     def save(self, *args, **kwargs):
         self.company_name_slug = slugify(self.company_name)
+        self.calendar_email = self.user.email
         super(ClientSetting, self).save(*args, **kwargs)
